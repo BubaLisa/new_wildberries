25008df7c4b60c57baabeba2e2ff36c34a47297a
Garpeta <kiragubert43@gmail.com>
2025-05-14 17:54:30 +0300

    upload

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index")
+    path("product/<int:product_id>", views.product_details, name="product_details"),
+    ]
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from .models import Product
+
+
 def index(request):
     products = Product.objects.all()
     context = {"products": products}
     return render(request, "app/index.html", context)
-# Create your views here.
+
+def products_details(request, product_id):
+    product = Product.objects.get(id=product_id)
+    categories = []
+    categories.append(product.categories)
+    cur_category
